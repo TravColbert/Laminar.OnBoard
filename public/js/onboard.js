66a39b3d35ca99ac8e6e9dcefe6ccf7fdcb7925e
Trav Colbert <trav.colbert@gmail.com>
2018-04-05 15:52:27 -0700

    added menu file, helpful client-side scripts

---
+++ public/js/onboard.js
@@ -0,0 +1,56 @@
+function ackMessage(messageNum) {
+  fetch('/messages/ack/' + messageNum,{
+    method:'GET',
+    credentials:'include'
+  }).
+    then(function(response) {
+      return response.json();
+    }).
+    then(function(json){
+      console.log(JSON.stringify(json));
+      doMessage(json);
+    });
+}
+
+function doMessage(json) {
+  document.getElementById("msg" + json.msgId).remove();
+}
+
+function goTo(target) {
+  window.location = target;
+}
+
+function dateInputExists() {
+  console.log("Checking for date picker input boxes...");
+  let box = document.getElementById("datetimeselect");
+  if(box!=null) {
+    console.log("...found one");
+    return box;
+  }
+  return false;
+}
+
+function makeDatePicker(element) {
+  if(element) flatpickr(element, {
+    enableTime: true
+  });
+  return;
+}
+
+function calculatedatetimestring(dateTimeString) {
+  let datetime = document.getElementById("datetime");
+  // window.alert(dateTimeString);
+  // let datetimestring = document.getElementById("datetimeselect").value;
+  datetime.value = Date.parse(dateTimeString);
+  return;
+}
+
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready();
