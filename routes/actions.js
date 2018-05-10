ac2c7ff0c3f8285d99141408a210f0ca428a5a07
Trav Colbert <trav.colbert@gmail.com>
2018-05-10 14:43:44 -0700

    removed extraneous commented line

--- routes/actions.js
+++ routes/actions.js
@@ -5,6 +5,5 @@ module.exports = function(app) {
   router.get('/create/role/',app.tools.checkAuthentication,app.controllers["roles"].createRoleForm);
   router.get('/create/domain/',app.tools.checkAuthentication,app.controllers["domains"].createDomainForm);
   router.get('/create/user/',app.tools.checkAuthentication,app.controllers["users"].createUserForm);
-  // router.get('/',app.tools.checkAuthentication,app.controllers["roles"].getRoles);
   return router;
 };
