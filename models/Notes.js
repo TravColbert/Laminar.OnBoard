876572043cfdd0bcb93f0fd3412436d54bba5199
Trav Colbert <trav.colbert@gmail.com>
2018-05-15 15:56:06 -0700

    adding note creation features

---
+++ models/Notes.js
@@ -0,0 +1,54 @@
+module.exports = function(Sequelize,app) {
+  return {
+    tablename:"notes",
+    schema:{
+      "name":{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      "description":{
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      "body":{
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      "appid":{
+        type: Sequelize.STRING
+      },
+      "domain":{
+        type: Sequelize.INTEGER,
+        references: {
+          model: app.models["domains"],
+          key: id,
+          deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+        }
+      },
+      "user":{
+        type: Sequelize.INTEGER,
+        references: {
+          model: app.model["users"],
+          key: id,
+          deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+        }
+      }
+    },
+    options:{
+      hooks:{
+        afterCreate:(note) => {
+          let myName = "note_model:afterCreate()";
+          app.log("creating unique app ID for note: " + note.id,myName,6);
+          note.appId = app.tools.generateString() + note.id;
+          note.update({"appid":noteappId})
+          .then((note) => {
+            app.log("unique app ID generated for note: " + note.id,myName,6);
+          })
+          .catch((err) => {
+            app.log(err.message,myName,4,"===>");
+          })
+        }
+      }
+    }
+  }
+}
