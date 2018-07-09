f199db4f32e57e57b607e491b9b94cf6b51c744b
Trav Colbert <trav.colbert@gmail.com>
2018-07-09 13:22:50 -0700

    added sample authorized element file

---
+++ elements/part_edit.js
@@ -0,0 +1,59 @@
+module.exports = {
+  role:["edit","all"],
+  element:{
+    id:"part_edit_button",
+    element:"a",
+    classlist:["btn","tertiary","layout-size-x1","buttonpartedit"],
+    content:"<i class=\"material-icons\">mode_edit</i><span>Edit Part</span>"
+  }
+
+  // "GET:/user/edit/:userId":{
+  //   role:"roleuser"
+  // },
+  // "GET:/part/edit/:partId":{
+  //   role:"roleparts"
+  // },
+  // "GET:/users/add":{
+  //   role:"roleuser"
+  // },
+  // "GET:/parts/add":{
+  //   role:"roleparts"
+  // },
+  // "user_edit":{
+  //   role:"roleuser",
+  //   element:{
+  //     id:"user_edit_button",
+  //     element:"a",
+  //     classlist:["btn","tertiary","layout-size-x1","buttonuseredit"],
+  //     content:"<i class=\"material-icons\">mode_edit</i><span>Edit User</span>"
+  //   }
+  // },
+  // "user_add":{
+  //   role:"roleuser",
+  //   element:{
+  //     id:"user_add_button",
+  //     element:"a",
+  //     classlist:["btn","normal"],
+  //     content:"<i class=\"material-icons\">add_circle_outline</i><span>Add User</span>"
+  //   }
+  // },
+  // "part_edit":{
+  //   role:"roleparts",
+  //   element:{
+  //     id:"part_edit_button",
+  //     element:"a",
+  //     classlist:["btn","tertiary","layout-size-x1","buttonpartedit"],
+  //     content:"<i class=\"material-icons\">mode_edit</i><span>Edit Part</span>"
+  //   }
+  // }
+  // ,
+  // "part_add":{
+  //   role:"roleparts",
+  //   element:{
+  //     id:"part_add_button",
+  //     element:"a",
+  //     classlist:["btn","normal"],
+  //     content:"<i class=\"material-icons\">add_circle_outline</i><span>Add Part</span>"
+  //   }
+  // }
+};
