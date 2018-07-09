826b030d5e89ec526a624e4dfcf0fc9f1c22351e
Trav Colbert <trav.colbert@gmail.com>
2018-07-09 13:33:51 -0700

    domain handling is better now

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,9 @@ module.exports = function(app) {
   // router.get('/create/',app.tools.checkAuthentication,app.tools.showPage("usercreate"));
   router.get('/',app.tools.checkAuthentication,app.controllers["users"].getUsers);
   router.get('/:id/',app.tools.checkAuthentication,app.controllers["users"].getUser);
-  router.get('/:id/actions/edit',app.tools.checkAuthentication,app.controllers["users"].editUserForm);
-  router.get('/:id/actions/switchdomain/:domainId',app.tools.checkAuthentication,app.tools.switchToDomain);
-  // router.get('/:id/actions/switchdomain/:domainId',app.tools.checkAuthentication,app.controllers["users"].switchCurrentDomain);
+  router.get('/:id/actions/edit/',app.tools.checkAuthentication,app.controllers["users"].editUserForm);
+  router.get('/:id/actions/switchdomain/:domainId/',app.tools.checkAuthentication,app.tools.switchToDomain);
+  router.get('/:id/actions/setdefaultdomain/:domainId/',app.tools.checkAuthentication,app.controllers["users"].setDefaultDomain);
   router.get('/:id/domains/',app.tools.checkAuthentication,app.controllers["users"].getDomainsByUserId);
   router.get('/:id/roles/',app.tools.checkAuthentication,app.controllers["users"].getRolesByUserId);
   router.post('/',app.tools.checkAuthentication,app.controllers["users"].createUser);
