8a0af3fb57320a3625e8d38adae81b63f944b942
Trav Colbert <trav.colbert@gmail.com>
2018-07-09 13:34:20 -0700

    commented out non-existent method for now

--- routes/domains.js
+++ routes/domains.js
@@ -7,6 +7,7 @@ module.exports = function(app) {
   router.get('/:id/actions/edit',app.tools.checkAuthentication,app.controllers["domains"].editDomainForm);
   router.get('/:id/roles/',app.tools.checkAuthentication,app.controllers["domains"].getRolesByDomainId);
   router.get('/:id/users/',app.tools.checkAuthentication,app.controllers["domains"].getUsersByDomainId);
+  // router.get('/:id/invite/',app.tools.checkAuthentication,app.controllers["domains"].invite);
   router.post('/',app.tools.checkAuthentication,app.controllers["domains"].create);
   router.post('/:id/',app.tools.checkAuthentication,app.controllers["domains"].editDomain);
   return router;
