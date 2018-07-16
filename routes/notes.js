ba3e2003490ad64e2308ed1ee0c4e116626f8771
Trav Colbert <trav.colbert@gmail.com>
2018-07-16 13:10:43 -0700

    changed method calls in route

--- routes/notes.js
+++ routes/notes.js
@@ -2,8 +2,8 @@ const express = require('express');
 var router = express.Router();
 
 module.exports = function(app) {
-  router.get('/',app.tools.checkAuthentication,app.controllers["notes"].getNotes);
-  router.get('/:id/',app.tools.checkAuthentication,app.controllers["notes"].getNote);
+  router.get('/',app.tools.checkAuthentication,app.controllers["notes"].gets);
+  router.get('/:id/',app.tools.checkAuthentication,app.controllers["notes"].get);
   router.get('/:id/actions/edit',app.tools.checkAuthentication,app.controllers["notes"].editNoteForm);
   router.post('/',app.tools.checkAuthentication,app.controllers["notes"].createNote);
   router.post('/:id/',app.tools.checkAuthentication,app.controllers["notes"].editNote);
