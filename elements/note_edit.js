1ee7fe42b87f79dfa6ae18575c8e5a560c5a2ce5
Trav Colbert <trav.colbert@gmail.com>
2018-07-16 13:15:00 -0700

    new elements for notes and users

---
+++ elements/note_edit.js
@@ -0,0 +1,9 @@
+module.exports = {
+  role:["edit","all"],
+  element:{
+    id:"note_edit_button",
+    element:"a",
+    classlist:["btn","tertiary","layout-size-x1","buttonnoteedit"],
+    content:"<i class=\"material-icons\">mode_edit</i><span>Edit Note</span>"
+  }
+};
---
+++ elements/user_add.js
@@ -0,0 +1,9 @@
+module.exports = {
+  role:["create","all"],
+  element:{
+    id:"user_add_button",
+    element:"a",
+    classlist:["btn","secondary","layout-size-x1","buttonadduser"],
+    content:"<i class=\"material-icons\">add_circle_outline</i><span>Add User</span>"
+  }
+}
