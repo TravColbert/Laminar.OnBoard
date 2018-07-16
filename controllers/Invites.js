f0aabbafffadd2700160be99f3d4f293332928c6
Trav Colbert <trav.colbert@gmail.com>
2018-07-16 10:11:05 -0700

    checking,adding,confirming invites

--- controllers/Invites.js
+++ controllers/Invites.js
@@ -43,6 +43,87 @@ module.exports = function(app,model) {
           reject(new Error("(" + myName + ") " + err.message));
         })
       })
+    },
+
+    getInviteQueryObj : function(userEmail, expirationHours) {
+      expirationHours = expirationHours || 72;
+      // Calculate the date some days ago
+      let timeLimit = new Date(new Date() - (1000 * 60 * 60 * expirationHours)); 
+      return {
+        where : {
+          "userEmail" : userEmail,
+          "accepted" : false,
+          "createdAt" : {
+            $gt : timeLimit
+          }  
+        }
+      }
+    },
+    attemptAccept : function(req,res,next) {
+      let myName = "attemptAccept";
+      app.log("Attempting to accept invite: " + req.params.id,myName,6);
+      req.appData.roleAppid = req.params.id;
+      req.appData.userEmail = req.session.user.email;
+      req.appData.view = "invitationaccept";
+      return next();
+    },
+    confirmAccept : function(req,res,next) {
+      let myName = "confirmAccept";
+      app.log("Attempting to confirm invite: " + req.params.id,myName,6);
+      let targetRole;
+      let invitation;
+      let searchObj = app.controllers[model].getInviteQueryObj(req.body.userEmail);
+      searchObj.where.pin = req.body.pin;
+      app.controllers[model].__get(searchObj)
+      .then(invites => {
+        if(invites.length!=1) return res.redirect("/");
+        app.log(JSON.stringify(invites),myName,6);
+        app.log("Invite accepted - setting things up",myName,5);
+        invitation = invites[0];
+        return app.controllers.roles.getRoleByAppId(invites[0].roleAppid.substring(0,12));
+      })
+      .then(role => {
+        if(!role) return res.send("No role found.");
+        app.log(JSON.stringify(role),myName,6);
+        app.log("Role found: " + role,myName,6);
+        targetRole = role;
+        app.log("Now, let's find the user: " + invitation.userEmail,myName,6);
+        return app.controllers.users.getUserByEmail(invitation.userEmail);
+      })
+      .then(users => {
+        if(users.length!=1) return res.send("Wrong number of users returned! That's a pretty big big");
+        app.log("Found user: " + users[0],myName,6);
+        app.log("Adding role: " + targetRole,myName,6);
+        let standardComment = "Invitation to role accepted by user";
+        let comment = standardComment + ": " + invitation.comment || standardComment;
+        return users[0].addRole(targetRole,{through:{"comment":comment}});
+      })
+      .then(result => {
+        app.log(result,myName,6);
+        app.log("Time to update the invitation: " + invitation,myName,6);
+        return invitation.update({"accepted":true});
+      })
+      .then(result => {
+        app.log("I think we're all committed now",myName,6);
+        app.log("Be good to load an explanatory vew.",myName,6);
+        req.appData.view = "invitationaccepted";
+        return next();
+      });      
+    },
+
+    checkInvites : function(userEmail,expirationHours) {
+      let myName = "checkInvites";
+      return new Promise((resolve,reject) => {
+        let searchObj = app.controllers[model].getInviteQueryObj(userEmail,expirationHours);
+        app.controllers.invites.__get(searchObj)
+        .then(invites => {
+          // app.log(invites,myName,6);
+          resolve(invites);
+        })
+        .catch(err => {
+          reject(err);
+        });
+      })
     }
   };
   return obj;
