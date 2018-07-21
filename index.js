d6d32275130f06171c04e5b1e363d9f093b36603
Trav Colbert <trav.colbert@gmail.com>
2018-07-21 00:58:32 +0000

    trying to integrate changes

--- index.js
+++ index.js
@@ -33,6 +33,18 @@ var sequelize = new Sequelize(
   }
 );
 
+
+app.mailjet = require('node-mailjet').connect(app.secrets["mail-api-key"], app.secrets["mail-api-secret"], {
+  url: app.locals.smtpServer, // default is the API url
+  version: 'v3.1', // default is '/v3'
+  perform_api_call: true // used for tests. default is true
+});
+
+const mailSender = app.mailjet.post('sender');
+
+// ... or...
+// .connect(app.secrets.mail-api-key, app.secrets.mail-api-secret);
+
 let sessionConfig = {
   secret:app.secrets.sessionSecret,
   resave:false,
