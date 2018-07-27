9798680012dca3a0294a5ba9f4fc1b52fbf3034e
Trav Colbert <trav.colbert@gmail.com>
2018-07-27 15:05:41 -0700

    new invite button

---
+++ elements/domain_invite.js
@@ -0,0 +1,9 @@
+module.exports = {
+  role:["create","all"],
+  element:{
+    id:"domain_invite_button",
+    element:"a",
+    classlist:["btn","secondary","layout-size-x1","buttondomaininvite"],
+    content:"<i class=\"material-icons\">add_circle_outline</i><span>Invite User to Domain</span>"
+  }
+}
