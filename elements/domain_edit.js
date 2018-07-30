f294ef1518b5175e456afff017d1bd1defcfc817
Trav Colbert <trav.colbert@gmail.com>
2018-07-29 17:21:48 -0700

    added edit button for domain

---
+++ elements/domain_edit.js
@@ -0,0 +1,9 @@
+module.exports = {
+  role:["edit","all"],
+  element:{
+    id:"domain_edit_button",
+    element:"a",
+    classlist:["btn","secondary","layout-size-x1","buttondomainedit"],
+    content:"<i class=\"material-icons\">mode_edit</i><span>Edit Domain</span>"
+  }
+}
