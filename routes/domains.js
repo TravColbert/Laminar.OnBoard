4596d28fb4c0412321a55fd3e3a86404618b5153
TravColbert <trav.colbert@gmail.com>
2018-12-24 11:12:23 -0800

    standardizing JS style

--- routes/domains.js
+++ routes/domains.js
@@ -1,14 +1,14 @@
-const express = require('express');
-var router = express.Router();
-const model = "domains";
+const express = require('express')
+var router = express.Router()
+const model = 'domains'
 
-module.exports = function(app) {
-  router.get('/',app.tools.checkAuthentication,app.controllers["domains"].getMyDomains);
-  router.get('/:id/',app.tools.checkAuthentication,app.controllers["domains"].getDomain);
-  router.get('/:id/actions/edit/',app.tools.checkAuthentication,app.controllers["domains"].editDomainForm);
-  router.get('/:id/roles/',app.tools.checkAuthentication,app.controllers["domains"].getRolesByDomainId);
-  router.get('/:id/users/',app.tools.checkAuthentication,app.controllers["domains"].getUsersByDomainId);
-  router.post('/',app.tools.checkAuthentication,app.controllers["domains"].createDomain);
-  router.post('/:id/',app.tools.checkAuthentication,app.controllers["domains"].editDomain);
-  return router;
-};
+module.exports = function (app) {
+  router.get('/', app.tools.checkAuthentication, app.controllers['domains'].getMyDomains)
+  router.get('/:id/', app.tools.checkAuthentication, app.controllers['domains'].getDomain)
+  router.get('/:id/actions/edit/', app.tools.checkAuthentication, app.controllers['domains'].editDomainForm)
+  router.get('/:id/roles/', app.tools.checkAuthentication, app.controllers['domains'].getRolesByDomainId)
+  router.get('/:id/users/', app.tools.checkAuthentication, app.controllers['domains'].getUsersByDomainId)
+  router.post('/', app.tools.checkAuthentication, app.controllers['domains'].createDomain)
+  router.post('/:id/', app.tools.checkAuthentication, app.controllers['domains'].editDomain)
+  return router
+}
