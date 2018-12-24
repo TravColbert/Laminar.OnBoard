5bb83c0a621b1d2e4628eaf7721e5eee675c63a0
TravColbert <trav.colbert@gmail.com>
2018-12-24 11:13:17 -0800

    standardizing JS layout

--- public/js/domainscreate.js
+++ public/js/domainscreate.js
@@ -1,30 +1,33 @@
-let urnField;
-let nameField;
+let urnField
+let nameField
 
-function suggestUrnField(e) {
-  let nameValue = e.currentTarget.value;
-  let newValue = nameValue.toLowerCase().replace(/ /g,"_");
-  urnField.value = newValue;
+function suggestUrnField (e) {
+  let nameValue = e.currentTarget.value
+  let newValue = nameValue
+    .toLowerCase()
+    .replace(/[\s]/g, '_')
+    .replace(/[\W]/g, '')
+  urnField.value = newValue
 }
 
-function formatUrnField(e) {
-  let origVal = e.currentTarget.value;
+function formatUrnField (e) {
+  let origVal = e.currentTarget.value
   // All lowercase, no illegal characters
-  console.log(origVal);
-  console.log(encodeURI(origVal));
+  console.log(origVal)
+  console.log(encodeURI(origVal))
 }
 
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn();
+function ready (fn) {
+  if (document.readyState !== 'loading') {
+    fn()
   } else {
-    document.addEventListener('DOMContentLoaded', fn);
+    document.addEventListener('DOMContentLoaded', fn)
   }
 }
 
 ready(() => {
-  nameField = document.getElementById("name");
-  urnField = document.getElementById("urn");
-  nameField.onkeyup = suggestUrnField;
-  urnField.onkeyup = formatUrnField;
-});
+  nameField = document.getElementById('name')
+  urnField = document.getElementById('urn')
+  nameField.onkeyup = suggestUrnField
+  urnField.onkeyup = formatUrnField
+})
