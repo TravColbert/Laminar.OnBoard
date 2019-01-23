95295bc5f716e007b6df681b96cfbeddd3da43fd
TravColbert <trav.colbert@gmail.com>
2019-01-23 12:59:59 -0800

    added new elements

---
+++ elements/role_add.js
@@ -0,0 +1,9 @@
+module.exports = {
+  role:["create","all"],
+  element:{
+    id:"role_add_button",
+    element:"a",
+    classlist:["btn","tertiary","layout-size-x1","buttonroleedit"],
+    content:"<i class=\"material-icons\">mode_edit</i><span>Add Role</span>"
+  }
+}
---
+++ elements/user_edit.js
@@ -0,0 +1,9 @@
+module.exports = {
+  role:["edit","all"],
+  element:{
+    id:"user_edit_button",
+    element:"a",
+    classlist:["btn","secondary","layout-size-x1","buttonadduser"],
+    content:"<i class=\"material-icons\">mode_edit</i><span>Edit User</span>"
+  }
+}
