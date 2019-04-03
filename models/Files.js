c9b624d4513726fc45472a05f1defeb4cf531e3d
TravColbert <trav.colbert@gmail.com>
2019-04-03 14:10:07 -0700

    added a file model mostly of additional media

---
+++ models/Files.js
@@ -0,0 +1,36 @@
+module.exports = function (Sequelize, app) {
+  return {
+    tablename: 'files',
+    schema: {
+      'name': {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      'description': {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      'appid': {
+        type: Sequelize.STRING
+      }
+    },
+    requiredFields: ['name'],
+    optionalFields: ['id', 'description', 'appid', 'domainId', 'userId'],
+    options: {
+      hooks: {
+        afterCreate: (file) => {
+          let myName = 'file_model:afterCreate()'
+          app.log('creating unique app ID for file: ' + file.name, myName, 6)
+          file.appId = app.tools.generateString() + file.id
+          file.update({ 'appid': file.appId })
+            .then((file) => {
+              app.log('unique app ID generated for file: ' + file.id, myName, 6)
+            })
+            .catch((err) => {
+              app.log(err.message, myName, 4, '===>')
+            })
+        }
+      }
+    }
+  }
+}
