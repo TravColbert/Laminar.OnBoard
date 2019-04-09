3e448b78cd944d7b36b1ffcdb2c276dae6b2304d
TravColbert <trav.colbert@gmail.com>
2019-04-09 10:33:21 -0700

    prepping for thumbnail creation

---
+++ elements/note_create.js
@@ -0,0 +1,11 @@
+module.exports = {
+  role: ['create', 'all'],
+  element: {
+    id: 'note_create_button',
+    element: 'input',
+    type: 'submit',
+    classlist: ['btn', 'secondary', 'layout-size-x1', 'buttonnoteadd'],
+    form: 'note-create',
+    content: '<i class="material-icons">add_circle_outline</i><span>Create Note</span>'
+  }
+}
