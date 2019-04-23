0092fb030a23e21543ba72a4fbbf7c971cc5486f
TravColbert <trav.colbert@gmail.com>
2019-04-23 15:59:16 -0700

    created an invites mixin

--- routes/domains.js
+++ routes/domains.js
@@ -3,12 +3,12 @@ var router = express.Router()
 const model = 'domains'
 
 module.exports = function (app) {
-  router.get('/', app.tools.checkAuthentication, app.controllers['domains'].getMyDomains)
-  router.get('/:id/', app.tools.checkAuthentication, app.controllers['domains'].getDomain)
-  router.get('/:id/actions/edit/', app.tools.checkAuthentication, app.controllers['domains'].editDomainForm)
-  router.get('/:id/roles/', app.tools.checkAuthentication, app.controllers['domains'].getRolesByDomainId)
-  router.get('/:id/users/', app.tools.checkAuthentication, app.controllers['domains'].getUsersByDomainId)
-  router.post('/', app.tools.checkAuthentication, app.controllers['domains'].createDomain)
-  router.post('/:id/', app.tools.checkAuthentication, app.controllers['domains'].editDomain)
+  router.get('/', app.tools.checkAuthentication, app.controllers[model].getMyDomains)
+  router.get('/:id/', app.tools.checkAuthentication, app.controllers[model].getDomain)
+  router.get('/:id/actions/edit/', app.tools.checkAuthentication, app.controllers[model].editDomainForm)
+  router.get('/:id/roles/', app.tools.checkAuthentication, app.controllers[model].getRolesByDomainId)
+  router.get('/:id/users/', app.tools.checkAuthentication, app.controllers[model].getUsersByDomainId)
+  router.post('/', app.tools.checkAuthentication, app.controllers[model].createDomain)
+  router.post('/:id/', app.tools.checkAuthentication, app.controllers[model].editDomain)
   return router
 }
