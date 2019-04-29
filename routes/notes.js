e2b329c4f240679b83650b0773d8ea7bacba371b
TravColbert <trav.colbert@gmail.com>
2019-04-29 08:36:23 -0700

    addition of middleware to switch domains upon request for singular model object

--- routes/notes.js
+++ routes/notes.js
@@ -2,9 +2,9 @@ const express = require('express')
 var router = express.Router()
 
 module.exports = function (app) {
-  // The authentication controls are in the gets and get methods here...
+  // The authentication controls are in the gets and get methods for notes
   router.get('/', app.controllers['notes'].gets)
-  router.get('/:id/', app.controllers['notes'].get)
+  router.get('/:id/', app.controllers['notes'].get, app.tools.triggerDomainSwitchBy('notes'))
   router.get('/:id/actions/edit', app.tools.checkAuthentication, app.controllers['notes'].editNoteForm)
   router.post('/', app.tools.checkAuthentication, app.controllers['notes'].createNote)
   router.post('/:id/', app.tools.checkAuthentication, app.controllers['notes'].editNote)
