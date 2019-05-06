3126555b8a1cc9044ee174406738d84575b7215d
TravColbert <trav.colbert@gmail.com>
2019-05-06 09:21:05 -0700

    standardized coding style

--- models/Notes.js
+++ models/Notes.js
@@ -1,49 +1,49 @@
 const showdown = require('showdown')
-module.exports = function(Sequelize,app) {
+module.exports = function (Sequelize, app) {
   return {
-    tablename:"notes",
-    schema:{
-      "name":{
+    tablename: 'notes',
+    schema: {
+      'name': {
         type: Sequelize.STRING,
         allowNull: false
       },
-      "description":{
+      'description': {
         type: Sequelize.STRING,
         allowNull: true
       },
-      "body":{
+      'body': {
         type: Sequelize.TEXT,
         allowNull: true
       },
-      "public":{
+      'public': {
         type: Sequelize.BOOLEAN,
         defaultValue: false
       },
-      "appid":{
+      'appid': {
         type: Sequelize.STRING
       }
     },
-    requiredFields: ["name"],
-    optionalFields: ["id","description","body","html","public","appid"],
-    options:{
+    requiredFields: ['name'],
+    optionalFields: ['id', 'description', 'body', 'html', 'public', 'appid'],
+    options: {
       getterMethods: {
-        html() {
+        html () {
           let converter = new showdown.Converter()
           return converter.makeHtml(this.body)
         }
       },
-      hooks:{
-        afterCreate:(note) => {
-          let myName = "note_model:afterCreate()";
-          app.log("creating unique app ID for note: " + note.id,myName,6);
-          note.appId = app.tools.generateString() + note.id;
-          note.update({"appid":note.appId})
-          .then((note) => {
-            app.log("unique app ID generated for note: " + note.id,myName,6);
-          })
-          .catch((err) => {
-            app.log(err.message,myName,4,"===>");
-          })
+      hooks: {
+        afterCreate: (note) => {
+          let myName = 'note_model:afterCreate()'
+          app.log('creating unique app ID for note: ' + note.id, myName, 6)
+          note.appId = app.tools.generateString() + note.id
+          note.update({ 'appid': note.appId })
+            .then((note) => {
+              app.log('unique app ID generated for note: ' + note.id, myName, 6)
+            })
+            .catch((err) => {
+              app.log(err.message, myName, 4, '===>')
+            })
         }
       }
     }
