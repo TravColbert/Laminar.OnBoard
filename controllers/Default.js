3b142007db4453a21064e0c579699cfd7a346f29
TravColbert <trav.colbert@gmail.com>
2019-05-15 10:51:05 -0700

    preparing default controller for prototype use

--- controllers/Default.js
+++ controllers/Default.js
@@ -1,71 +1,70 @@
-module.exports = function(app,model) {
-  if(!model) return false;
-  let myName = model + "Controller";
-  let myModel = model;
+module.exports = function (app, model) {
+  if (!model) return false
+  let myModel = model
   return {
-    create : function(model,obj) {
-      let myName = "create(default)";
-      return new Promise((resolve,reject) => {
-        if(!app.models.hasOwnProperty(model)) reject(new Error("Model: " + model + " does not exist"));
+    create: function (model, obj) {
+      let myName = 'create(default)';
+      return new Promise((resolve, reject) => {
+        if (!app.models.hasOwnProperty(model)) reject(new Error('Model: ' + model + ' does not exist'))
         // app.log("Creating " + model + ": " + JSON.stringify(obj),myName,6);
         app.models[model].create(obj)
-        .then(item => {
+          .then(item => {
           // app.log(JSON.stringify(item),myName,6);
-          resolve(item);
-        })
-        .catch(err => {
-          app.log(err.message,myName,5);
-          reject(err);
-        });
-      });
+            resolve(item)
+          })
+          .catch(err => {
+            app.log(err.message, myName, 5)
+            reject(err)
+          })
+      })
     },
-    get : function(model,obj) {
-      let myName = "get(default)";
-      return new Promise((resolve,reject) => {
-        if(!app.models.hasOwnProperty(model)) reject(new Error("Model: " + model + " does not exist"));
+    get: function (model, obj) {
+      let myName = 'get(default)';
+      return new Promise((resolve, reject) => {
+        if (!app.models.hasOwnProperty(model)) reject(new Error('Model: ' + model + ' does not exist'))
         // app.log("Getting " + model + ": " + JSON.stringify(obj),myName,6);
         app.models[model].findAll(obj)
-        .then(items => {
-          if(items===null) resolve([]);
+          .then(items => {
+            if (items === null) resolve([])
           // app.log("Result: " + JSON.stringify(items),myName,6);
-          resolve(items);
+          resolve(items)
         }).catch(err => {
-          app.log(err.message,myName,5);
-          reject(err);
-        });
-      });
-    },
-    update : function(model,obj) {
-      let myName = "update(default)";
-      return new Promise((resolve,reject) => {
-        if(!app.models.hasOwnProperty(model)) reject(new Error("Model: " + model + " does not exist"));
-        // app.log("Updating " + model + ": " + obj,myName,6);
-        app.models[model].update(obj.values,obj.options)
-        .then(items => {
-          // app.log(JSON.stringify(items),myName,6);
-          resolve(items);
-        })
-        .catch(err => {
-          app.log(err.message,myName,5);
-          reject(err);
+            app.log(err.message, myName, 5)
+          reject(err)
         })
       })
     },
-    delete : function(model,obj) {
-      let myName = "delete(default)";
-      return new Promise((resolve,reject) => {
-        if(!app.models.hasOwnProperty(model)) reject(new Error("Model: " + model + " does not exist"));
+    update: function (model, obj) {
+      let myName = 'update(default)';
+      return new Promise((resolve, reject) => {
+        if (!app.models.hasOwnProperty(model)) reject(new Error('Model: ' + model + ' does not exist'))
+        // app.log("Updating " + model + ": " + obj,myName,6);
+        app.models[model].update(obj.values, obj.options)
+          .then(items => {
+          // app.log(JSON.stringify(items),myName,6);
+            resolve(items)
+        })
+          .catch(err => {
+            app.log(err.message, myName, 5)
+          reject(err)
+        })
+      })
+    },
+    delete: function (model, obj) {
+      let myName = 'delete(default)';
+      return new Promise((resolve, reject) => {
+        if (!app.models.hasOwnProperty(model)) reject(new Error('Model: ' + model + ' does not exist'))
         // app.log("Deleting " + model + ": " + JSON.stringify(obj),myName,6);
         app.models[model].destroy(obj)
-        .then(items => {
+          .then(items => {
           // app.log(JSON.stringify(items),myName,6);
-          resolve(items);
+            resolve(items)
         })
-        .catch(err => {
-          app.log(err.message,myName,5);
-          reject(err);
+          .catch(err => {
+            app.log(err.message, myName, 5)
+          reject(err)
         })
       })
     }
-  };
+  }
 };
