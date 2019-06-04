d49597a03951547ae899245e83170f084e1c4a51
TravColbert <trav.colbert@gmail.com>
2019-06-04 15:14:27 -0700

    fixed login problem - no trailing slash

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.homeModule = false
 if (app.locals.hasOwnProperty('homeModule')) {
   if (app.locals.homeModule !== false && app.locals.homeModule !== null) {
     app.log('Including home module: ' + app.locals.homeModule, myName, 6)
-    app.homeModule = require(path.join(app.locals.modulesDir, app.locals.homeModule))(app)
+    app.homeModule = require(path.join(__dirname, app.locals.modulesDir, app.locals.homeModule))(app)
   }
 }
 
