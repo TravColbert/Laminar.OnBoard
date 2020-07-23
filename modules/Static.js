5bf78d5cc20d41ed960043ba732f459760b2c749
Travis Colbert <trav.colbert@gmail.com>
2020-07-23 19:53:46 +0000

    static file-handling off-loaded to module

---
+++ modules/Static.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const fs = require('fs')
+
+module.exports = function(app,express) {
+  let myName = `Laminar.Static`
+
+  let staticOptions = app.locals.staticOptions || {}
+  app.use(express.static(path.join(app.cwd, app.locals.staticDir),staticOptions))
+
+  let staticFiles = [
+    {req:'/favicon.ico', file:app.locals.favicon || 'public/img/favicon-laminar.ico'},
+    {req:'/robots.txt', file:app.locals.robots || 'public/robots.txt'},
+    {req:'/sitemap.xml', file:app.locals.sitemap || 'public/sitemap.xml'}
+  ]
+  staticFiles.forEach(fileObj => {
+    app.log(`Setting static file: ${fileObj.req}`, myName, 5)
+    app.use(fileObj.req, express.static(path.join(app.cwd, fileObj.file)), function (req, res, next) {
+      let message = `Could not serve static file: ${path.join(app.cwd, fileObj.file)}`
+      app.log(message)
+      res.end()
+    })
+  })
+  
+  return app
+}
