b1d7568f1298e0661652f5f153c354053e2d8df2
Travis Colbert <trav.colbert@gmail.com>
2020-07-24 20:39:21 +0000

    added hello method and cleaned out comments

--- controllers/Default.js
+++ controllers/Default.js
@@ -1,36 +1,32 @@
 module.exports = function (app, model) {
   if (!model) return false
+  let myName = model + 'Controller'
   return {
+    hello: function () {
+      app.log(`Hello from ${myName}`, myName, 6)
+      return true
+    },
     create: function (model, obj) {
       let myName = 'create(default)';
       if (!app.models.hasOwnProperty(model)) return new Error('Model: ' + model + ' does not exist')
       app.log(`Creating ${model}`, myName, 6)
       return app.models[model].create(obj)
-      // return new Promise((resolve, reject) => {
-      //   // app.log("Creating " + model + ": " + JSON.stringify(obj),myName,6);
-      //   app.models[model].create(obj)
-      //     .then(item => {
-      //     // app.log(JSON.stringify(item),myName,6);
-      //       resolve(item)
-      //     })
-      //     .catch(err => {
-      //       app.log(err.message, myName, 5)
-      //       reject(err)
-      //     })
-      // })
+      .catch(err => {
+        app.log(err.message, myName, 5)
+        reject(err)
+      })
     },
     get: function (model, obj) {
       let myName = 'get(default)';
       return new Promise((resolve, reject) => {
         if (!app.models.hasOwnProperty(model)) reject(new Error('Model: ' + model + ' does not exist'))
-        // app.log("Getting " + model + ": " + JSON.stringify(obj),myName,6);
         app.models[model].findAll(obj)
-          .then(items => {
-            if (items === null) resolve([])
-          // app.log("Result: " + JSON.stringify(items),myName,6);
+        .then(items => {
+          if (items === null) resolve([])
           resolve(items)
-        }).catch(err => {
-            app.log(err.message, myName, 5)
+        })
+        .catch(err => {
+          app.log(err.message, myName, 5)
           reject(err)
         })
       })
@@ -39,14 +35,12 @@ module.exports = function (app, model) {
       let myName = 'update(default)';
       return new Promise((resolve, reject) => {
         if (!app.models.hasOwnProperty(model)) reject(new Error('Model: ' + model + ' does not exist'))
-        // app.log("Updating " + model + ": " + obj,myName,6);
         app.models[model].update(obj.values, obj.options)
-          .then(items => {
-          // app.log(JSON.stringify(items),myName,6);
-            resolve(items)
+        .then(items => {
+          resolve(items)
         })
-          .catch(err => {
-            app.log(err.message, myName, 5)
+        .catch(err => {
+          app.log(err.message, myName, 5)
           reject(err)
         })
       })
@@ -55,14 +49,12 @@ module.exports = function (app, model) {
       let myName = 'delete(default)';
       return new Promise((resolve, reject) => {
         if (!app.models.hasOwnProperty(model)) reject(new Error('Model: ' + model + ' does not exist'))
-        // app.log("Deleting " + model + ": " + JSON.stringify(obj),myName,6);
         app.models[model].destroy(obj)
-          .then(items => {
-          // app.log(JSON.stringify(items),myName,6);
-            resolve(items)
+        .then(items => {
+          resolve(items)
         })
-          .catch(err => {
-            app.log(err.message, myName, 5)
+        .catch(err => {
+          app.log(err.message, myName, 5)
           reject(err)
         })
       })
